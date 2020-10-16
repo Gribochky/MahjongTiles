906eba5a8660f2bc1f8970059644cd98d28d3526
Gribochky <72981773+Gribochky@users.noreply.github.com>
2020-10-16 21:38:12 +0800

    Add files via upload

---
+++ java/com/example/mahjongtiles/Tile.java
@@ -0,0 +1,15 @@
+package com.example.mahjongtiles;
+
+public class Tile {
+
+    private String value;
+
+    public Tile(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
